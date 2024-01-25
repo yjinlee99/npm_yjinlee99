7f75375af8c5c67340a18739dac40bb4a14063f9
yjinnote <yjinlee99@gmail.com>
2024-01-25 17:43:15 +0900

    third deploy

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 #!/usr/bin/env node
 
-console.log('hello yjinlee99');
+
+class Random{
+	constructor(argv) {
+		this.argv = argv;
+	}
+	say() {
+		console.log('발표자 :' + this.argv[Math.floor(Math.random()*this.argv.length)]);
+	}
+
+}
+
+class RandomPrint extends Random {
+	constructor(argv){
+		super(argv.slice(2));
+	}
+}
+
+var random = new RandomPrint(process.argv);
+if(random.argv.length<2) {
+	console.log('팀원은 2명 이상 이여야 합니다');
+	process.exit();
+}
+
 console.log(`
                                      :+@@@@@@@@@@@*:                                                
                                  %@@@:             .@@@%                                            
@@ -33,3 +55,6 @@ console.log(`
                                              .*#+                                                   
                                                                  
 ` );
+
+
+random.say();
