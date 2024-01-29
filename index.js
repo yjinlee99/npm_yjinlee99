e2ec23a7145b8393e9f09e33eadbf0ecce7a90d8
yjinnote <yjinlee99@gmail.com>
2024-01-29 11:52:35 +0900

    리팩토링 완료

--- index.js
+++ index.js
@@ -2,23 +2,23 @@
 
 
 class Random{
-	constructor(argv) {
+	constructor(...argv) {
 		this.argv = argv;
 	}
 	say() {
-		console.log('발표자 :' + this.argv[Math.floor(Math.random()*this.argv.length)]);
+		console.log('발표자 :' + this.argv[0][Math.floor(Math.random()*this.argv[0].length)]);
 	}
 
 }
 
 class RandomPrint extends Random {
-	constructor(argv){
+	constructor(argv = process.argv){
 		super(argv.slice(2));
 	}
 }
 
-var random = new RandomPrint(process.argv);
-if(random.argv.length<2) {
+var random = new RandomPrint();
+if(random.argv[0].length<2) {
 	console.log('팀원은 2명 이상 이여야 합니다');
 	process.exit();
 }
